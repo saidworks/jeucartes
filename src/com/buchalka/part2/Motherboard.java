0af73cbd88410fe92ac97ad98d278c4f9c98cff1
Said ZITOUNI <zitouni.sd@gmail.com>
2021-12-11 13:22:17 +0100

    ignore eclipse files

--- src/com/buchalka/part2/Motherboard.java
+++ src/com/buchalka/part2/Motherboard.java
@@ -15,6 +15,46 @@ public class Motherboard {
 		this.cardSlots = cardSlots;
 		this.bios = bios;
 	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public String getManufacturer() {
+		return manufacturer;
+	}
+
+	public void setManufacturer(String manufacturer) {
+		this.manufacturer = manufacturer;
+	}
+
+	public int getRamSlots() {
+		return ramSlots;
+	}
+
+	public void setRamSlots(int ramSlots) {
+		this.ramSlots = ramSlots;
+	}
+
+	public int getCardSlots() {
+		return cardSlots;
+	}
+
+	public void setCardSlots(int cardSlots) {
+		this.cardSlots = cardSlots;
+	}
+
+	public String getBios() {
+		return bios;
+	}
+
+	public void setBios(String bios) {
+		this.bios = bios;
+	}
 	
 	
 }
